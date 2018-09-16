382ebfa1d56edd603bd6262484ddbfaf05205f0a
joshem <joshe@bu.edu>
2018-09-16 18:39:21 -0400

    Edited Twit Image Retriever

--- Project1/TwitterImages.py
+++ Project1/TwitterImages.py
@@ -5,24 +5,26 @@ import wget
 import json
 
 import getPassword
-
+myScreenname = 'JoshMan63909666'
+twitCnt = 10
+#put it all in try-except block for error handling
 try:
+	#OAuthHandler stuff
 	auth = tweepy.OAuthHandler(getPassword.API_KEY, getPassword.API_SECRET_KEY)
 	auth.set_access_token(getPassword.ACCESS_TOKEN, getPassword.ACCESS_TOKEN_SECRET)
 	myAccnt = tweepy.API(auth)
-
-	myTweets = myAccnt.user_timeline(screen_name='JoshMan63909666',count=10)
-	#myTweets = []
-	#for tweet in tweepy.Cursor(myAccnt.user_timeline).items(10)
-	#	myTweets.append(tweet)	
+	#grab tweets from my account
+	myTweets = myAccnt.user_timeline(screen_name=myScreenname,count=twitCnt)
+	#extract the photos from these Tweets
 	myPics = []
 	for tweet in myTweets:
-		media = tweet.entities.get('media',[])
+		media = tweet.entities.get('media'git,[])
 		if(len(media)>0):
 			myPics.append(media[0]['media_url'])
 	print(myPics)
+	#using wget, download these pics into a folder, using a standardized naming system
+	#standardized naming allows for predictable file names, makes iteration easier in later sections
 	cnt = 7000
-
 	for pic in myPics:
 		picPath = "./TwitterPics/TwitPic"
 		cntStr = str(cnt)
